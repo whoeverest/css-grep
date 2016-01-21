b96649d58934ffbb209cc6f94841c364a3e1437e
Andrej T <andrejtrajchevski@gmail.com>
2016-01-21 14:09:32 +0100

    init

---
+++ css-grep.py
@@ -0,0 +1,15 @@
+import sys
+from lxml.etree import fromstring, tostring
+from lxml.cssselect import CSSSelector
+
+if len(sys.argv) < 2:
+  print 'Usage:\n  cssgrep "cat example.html | cssgrep "a.something > div"'
+  exit(1)
+
+content = sys.stdin.read() or '';
+parsed = fromstring(content);
+selector = CSSSelector(sys.argv[1]);
+results = selector(parsed)
+
+for node in results:
+  print tostring(node, pretty_print=True).strip()
